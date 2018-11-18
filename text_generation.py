36a28594f22f121a9c0b1c580945b773f1365202
dylanross <dylan.martin.ross@gmail.com>
2018-11-18 18:27:09 +0000

    better pre-processing

--- text_generation.py
+++ text_generation.py
@@ -8,40 +8,6 @@ from textgenrnn import textgenrnn
 
 
 """
-Load PDF text sources, pre-process, then save.
-"""
-
-# The Transsexual Empire: The Making of the Shemale by Janice Raymond
-#print("loading pdf (The Transsexual Empire)...")
-#text = textract.process("./the-transsexual-empire.pdf")
-#text = text.decode("utf-8")
-#text = text.lower()
-
-#print("saving txt (The Transsexual Empire)...")
-#text_file = open("./the_transsexual_empire.txt", "w")
-#text_file.write(text)
-#text_file.close()
-# TODO edit this txt file by hand to remove all instances of "the transsexual
-# empire"?
-
-
-# The Man Who Would Be Queen by J. Michael Bailey
-#print("loading pdf (The Man Who Would Be Queen)...")
-#text = textract.process("./the-man-who-would-be-queen.pdf")
-#text = text.decode("utf-8")
-#text = text.lower()
-#
-#print("saving txt (The Man Who Would Be Queen)...")
-#text_file = open("./the_man_who_would_be_queen.txt", "w")
-#text_file.write(text)
-#text_file.close()
-# NOTE I edited this txt file by hand to remove copyright info that was listed
-# on each page
-# TODO edit this txt file by hand to remove all instances of "the man who would
-# be queen"
-
-
-"""
 Build and train models.
 """
 
