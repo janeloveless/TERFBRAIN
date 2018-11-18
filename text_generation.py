ca607d80750a84af04f1e5686b8fb24cc1508ba3
dylanross <dylan.martin.ross@gmail.com>
2018-11-18 18:04:28 +0000

    improved preprocessing on text files

--- text_generation.py
+++ text_generation.py
@@ -42,15 +42,25 @@ Load PDF text sources, pre-process, then save.
 
 
 """
-Build model.
+Build and train models.
 """
 
+num_epochs=50
+
+print("Building model for The Man Who Would Be Queen...")
 textgen = textgenrnn()
+print("Training model for The Man Who Would Be Queen...")
+textgen.train_from_file("./the_man_who_would_be_queen.txt", num_epochs=num_epochs)
+textgen.save("./the_man_who_would_be_queen_weights_epoch" + str(num_epochs) + ".hdf5")
 
+print("Building model for The Transsexual Empire...")
+textgen = textgenrnn()
+print("Training model for The Transsexual Empire...")
+textgen.train_from_file("./the_transsexual_empire.txt", num_epochs=1)
+textgen.save("./the_transsexual_empire_weights_epoch" + str(num_epochs) + ".hdf5")
 
-"""
-Train model.
-"""
-
-textgen.train_from_file("./the_man_who_would_be_queen.txt", num_epochs=1)
-textgen.save("./the_man_who_would_be_queen_weights_epoch001.hdf5")
+print("Building the TERFBRAIN...")
+textgen = textgenrnn()
+print("Training model on The Man Who Would Be Queen and The Transsexual Empire...")
+textgen.train_from_file("./corpus.txt", num_epochs=num_epochs)
+textgen.save("./TERFBRAIN_weights_epoch" + str(num_epochs) + ".hdf5")
