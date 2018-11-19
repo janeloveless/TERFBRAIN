f5df3595ad14b1dd9922894e3a182a01d227bba7
dylanross <dylan.martin.ross@gmail.com>
2018-11-19 10:30:55 +0000

    50 epochs training

--- text_generation.py
+++ text_generation.py
@@ -11,22 +11,22 @@ from textgenrnn import textgenrnn
 Build and train models.
 """
 
-num_epochs=50
+num_epochs = 50
 
 print("Building model for The Man Who Would Be Queen...")
-textgen = textgenrnn()
+textgen1 = textgenrnn()
 print("Training model for The Man Who Would Be Queen...")
-textgen.train_from_file("./the_man_who_would_be_queen.txt", num_epochs=num_epochs)
-textgen.save("./the_man_who_would_be_queen_weights_epoch" + str(num_epochs) + ".hdf5")
+textgen1.train_from_file("./the_man_who_would_be_queen.txt", num_epochs=num_epochs)
+textgen1.save("./the_man_who_would_be_queen_weights_epoch" + str(num_epochs) + ".hdf5")
 
 print("Building model for The Transsexual Empire...")
-textgen = textgenrnn()
+textgen2 = textgenrnn()
 print("Training model for The Transsexual Empire...")
-textgen.train_from_file("./the_transsexual_empire.txt", num_epochs=1)
-textgen.save("./the_transsexual_empire_weights_epoch" + str(num_epochs) + ".hdf5")
+textgen2.train_from_file("./the_transsexual_empire.txt", num_epochs=1)
+textgen2.save("./the_transsexual_empire_weights_epoch" + str(num_epochs) + ".hdf5")
 
 print("Building the TERFBRAIN...")
-textgen = textgenrnn()
+textgen3 = textgenrnn()
 print("Training model on The Man Who Would Be Queen and The Transsexual Empire...")
-textgen.train_from_file("./corpus.txt", num_epochs=num_epochs)
-textgen.save("./TERFBRAIN_weights_epoch" + str(num_epochs) + ".hdf5")
+textgen3.train_from_file("./corpus.txt", num_epochs=num_epochs)
+textgen3.save("./TERFBRAIN_weights_epoch" + str(num_epochs) + ".hdf5")
