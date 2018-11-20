a918ade224488e7ce8bfc4fbc03ab9aa4b01b264
dylanross <dylan.martin.ross@gmail.com>
2018-11-20 10:40:53 +0000

    blah

--- TERFBRAIN.py
+++ TERFBRAIN.py
@@ -11,6 +11,6 @@ Build models.
 """
 
 print("Building model for The Man Who Would Be Queen...")
-textgen1 = textgenrnn("./weights/the_man_who_would_be_queen_weights_epoch50.hdf5")
-textgen2 = textgenrnn("./weights/the_transsexual_empire_weights_epoch50.hdf5")
-#textgen3 = textgenrnn("./weights/TERFBRAIN_weights_epoch50.hdf5")
+#textgen1 = textgenrnn("./weights/the_man_who_would_be_queen_weights_epoch50.hdf5")
+#textgen2 = textgenrnn("./weights/the_transsexual_empire_weights_epoch50.hdf5")
+textgen = textgenrnn("./weights/TERFBRAIN_weights_epoch50.hdf5")
